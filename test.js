baa1cc6eb4d0aead2ae3296acc1d852ddb503a69
zhouzhe <zzhe828@gmail.com>
2024-08-08 00:45:41 +0800

    0807

--- test.js
+++ test.js
@@ -1,46 +1,40 @@
-document
-.querySelector(".card-right-arrow")
-.addEventListener("click", rightSlides);
 var i = 0;
+
+document
+  .querySelector(".card-right-arrow")
+  .addEventListener("click", rightSlides);
 
 function rightSlides() {
   var cardGroup = document.querySelector(".card-photo-group");
   var maxClicks = cardGroup.querySelectorAll("div").length;
-  i++;
+  if (i <= maxClicks - 1) {
+    i++;
+  }  
+  console.log(i);
 
-  if (i > maxClicks-1) {
+  if (i > maxClicks - 1) {
     document.querySelector(".card-right-arrow").disabled = true;
+    document.querySelector(".card-left-arrow").disabled = false;
   } else {
-    console.log("ok");
     cardGroup.style.transform += "translateX(-490px)";
   }
 }
 document
-.querySelector(".card-left-arrow")
-.addEventListener("click", leftSlides);
-var j = 0;
+  .querySelector(".card-left-arrow")
+  .addEventListener("click", leftSlides);
 
 function leftSlides() {
   var cardGroup = document.querySelector(".card-photo-group");
   var maxClicks = cardGroup.querySelectorAll("div").length;
-  j++;
+  if (i >0) {
+    i--
+  }
+  console.log(i);
 
-  
-  if (j > maxClicks-1) {
+  if (i <= 0) {
     document.querySelector(".card-left-arrow").disabled = true;
   } else {
-    console.log("ok");
+    document.querySelector(".card-right-arrow").disabled = false;
     cardGroup.style.transform += "translateX(490px)";
   }
 }
-
-// if (slideIndex == 1) {
-//   slide.style.transform = "translateX(-490px)";
-// } else if (slideIndex == 2) {
-//   slide.style.transform = "translateX(-980px)";
-// } else if (slideIndex == 3) {
-//   slide.style.transform = "translateX(-1470px)";
-// } else {
-//   slide.style.transform = "translateX(-1960px)";
-// }
-// slideIndex++;
